fc4856355f01341e38a3e82bd1d797ffefc9f92d
Tabitha Peter <tabitha-peter@uiowa.edu>
2025-07-28 10:27:07 -0500

    add script comparing plmm and glmnet prediction performance using the PennCath data

--- scripts/obj4-glmnet-v-plmmr/analysis.R
+++ scripts/obj4-glmnet-v-plmmr/analysis.R
@@ -1,17 +1,29 @@
-# TKP - July 2025
-# Objective - create candidate models using plmmr and glmnet, then use
-#   CV for model selection.
-# Will use the PennCath data again here.
-# This script assumes that the model fitting from objective 1 has already been done.
+# Preamble: Load necessary libraries ------------------------
+library(glmnet)
 
-# set up ------------------------
-design_wo_std <- readRDS("results/n1401_p700K/penncath_wo_std.rds")
+# Set up file paths and directories ------------------------
+# Define base directories for inputs and outputs
+# This makes it easy to change the root folder structure in one place.
+results_dir <- "results"
+obj4_dir <- file.path(results_dir, "obj4")
 
+# Create the output directory if it doesn't already exist
+# The 'recursive = TRUE' argument creates any necessary parent directories.
+if (!dir.exists(obj4_dir)) {
+  dir.create(obj4_dir, recursive = TRUE)
+}
+
+# Construct the full path to the input file
+penncath_file <- file.path(results_dir, "n1401_p700K", "penncath_wo_std.rds")
+design_wo_std <- readRDS(penncath_file) # this has the processed data matrix - see setup.R
+
+# Attach the big.matrix data
 X <- bigmemory::attach.big.matrix(design_wo_std$subset_X)
 
-# create train and test sets
-# build the model on 1000 observations; hold out the other 401 for evaluating prediction
-set.seed(37075)
+
+# Create train and test sets ------------------------
+# Build the model on 1000 observations; hold out the other 401 for prediction
+set.seed(37075) # Set seed for reproducible random sampling
 test_idx <- sample(1:nrow(X), 401) |> sort()
 test_X <- X[test_idx,]
 train_X <- X[-test_idx,]
@@ -19,52 +31,81 @@ train_X <- X[-test_idx,]
 test_y <- design_wo_std$y[test_idx]
 train_y <- design_wo_std$y[-test_idx]
 
-# PLMM ------------
+# PLMM ------------------------
 train_design <- plmmr::create_design(X = train_X,
                                      y = train_y)
 
+# Use file.path to construct the save path
+# Note: The plmmr function adds the '.rds' extension automatically.
+cv_fit_path <- file.path(obj4_dir, "cv_fit")
+
 cvres_plmm <- plmmr::cv_plmm(design = train_design,
                              trace = TRUE,
-                             save_rds = "results/obj4/cv_fit")
+                             save_rds = cv_fit_path)
 
-# for reading in results from RDS files, use the lines below:
-# cvres_plmm <- readRDS("results/obj4/cv_fit.rds")
+# For reading in results from RDS files, you can use the lines below:
+# cv_fit_rds_path <- file.path(obj4_dir, "cv_fit.rds")
+# cvres_plmm <- readRDS(cv_fit_rds_path)
 # cvres_plmm <- structure(cvres_plmm, class="cv_plmm")
 
 summary(cvres_plmm)
 plmm_beta <- coef(cvres_plmm)
-plmm_nz <- which(abs(plmm_beta[,1]) > 0.00000001)
-plmm_beta[plmm_nz,]
+names(plmm_beta) <- c("(Intercept)",design_wo_std$std_X_colnames)
+plmm_nz <- which(abs(plmm_beta) > 1e-8) # Using 1e-8 for numeric precision
+plmm_beta[plmm_nz] |> round(3)
 
-# lasso only ------------
-library(glmnet)
+
+# Lasso only ------------------------
 glmnet_fit <- glmnet::cv.glmnet(x = train_X,
                                 y = train_y,
                                 # use same settings as in PLMM approach
                                 nfolds = 5,
                                 lambda = cvres_plmm$lambda,
-                                penalty.factor = design_wo_std$penalty_factor,
+                                penalty.factor = train_design$penalty_factor,
                                 # show trace
-                                trace.it = T)
+                                trace.it = TRUE)
 
-saveRDS(glmnet_fit, "results/obj4/glmnet_fit.rds")
+# Construct the save path for the glmnet model object
+glmnet_fit_path <- file.path(obj4_dir, "glmnet_fit.rds")
+saveRDS(glmnet_fit, glmnet_fit_path)
 
 glmnet_beta <- coef(glmnet_fit, s = "lambda.min")
 rownames(glmnet_beta) <- c('(Intercept)', design_wo_std$std_X_colnames)
-glmnet_nz <- which(abs(glmnet_beta[,1]) > 0.00000001)
-glmnet_beta[glmnet_nz,]
+glmnet_nz <- which(abs(glmnet_beta[,1]) > 1e-8)
+glmnet_beta[glmnet_nz,] |> round(3)
 
 
-# compare predictions -----------
-# null model (predict mean for all outcomes)
-(null_mspe <- crossprod(design$y - rep(mean(design$y, length(design$y))))/length(design$y))
+# Compare predictions on the test set ------------------------
+# Null model (predicting the mean of the training data)
+null_pred <- rep(mean(train_y), length(test_y))
+(null_mspe <- mean((test_y - null_pred)^2))
 
-# PLMM
-plmm_yhat <- readRDS("results/obj4/cv_fit_yhat.rds")
-pred_plmm <- plmm_yhat[,cvres_plmm$min]
-(plmm_mspe <- crossprod(design$y - cvpred_plmm[cvres_plmm$min])/length(design$y))
+# PLMM predictions
+plmm_fit <- plmmr::plmm(design = train_design,
+                        lambda = cvres_plmm$lambda_min,
+                        trace = TRUE)
+pred_plmm <- predict(plmm_fit, newX = test_X, X = train_X)
+(plmm_mspe <- mean((test_y - pred_plmm)^2))
 
-# glmnet
-pred_glmnet <- predict(glmnet_fit, newx = ram_X, s = "lambda.min")
-saveRDS(pred_glmnet, "results/obj4/pred_glmnet.rds")
-(glmnet_mspe <- crossprod(design$y - pred_glmnet)/length(design$y))
+
+# GLMNET predictions
+pred_glmnet <- predict(glmnet_fit, newx = test_X, s = "lambda.min")
+
+# Construct the save path for the predictions
+pred_glmnet_path <- file.path(obj4_dir, "pred_glmnet.rds")
+saveRDS(pred_glmnet, pred_glmnet_path)
+
+# Calculate Mean Squared Prediction Error on the test set
+(glmnet_mspe <- mean((test_y - pred_glmnet)^2))
+
+
+# Create table of results ------------------------------
+results_df <- data.frame(
+  model = c("Null", "GLMNET", "PLMM"),
+  MSPE = c(null_mspe, glmnet_mspe, plmm_mspe) |> round(digits = 3),
+  NVAR = c(NA_integer_,
+           length(glmnet_beta[glmnet_nz,]) - 1,
+           length(plmm_beta[plmm_nz]) - 1)
+)
+
+print(results_df)
